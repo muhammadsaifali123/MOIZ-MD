8f72f75fd831c4cd71a7a9746323802dd37b5673
muhammadsaifali123 <94912062+muhammadsaifali123@users.noreply.github.com>
2025-04-13 22:53:33 +0500

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/7zfdcq.jpg` }, // Replace with privacy-themed image if available
+                image: { url: `https://files.catbox.moe/afsrye.jpg` }, // Replace with privacy-themed image if available
                 caption: privacyMenu,
                 contextInfo: {
                     mentionedJid: [m.sender],
