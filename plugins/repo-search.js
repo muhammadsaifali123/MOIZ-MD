a17f3553531685ad9332b1d9cbcee0cd5ee41c87
muhammadsaifali123 <94912062+muhammadsaifali123@users.noreply.github.com>
2025-04-13 23:04:46 +0500

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by JawadTechX*`;
+    responseMsg += `\n> *©  Pᴏᴡᴇʀᴇᴅ Bʏ ᴍᴏɪZ*`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
