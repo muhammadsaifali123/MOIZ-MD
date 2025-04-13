38758dd3c546f54f820117782e41fb733221038b
muhammadsaifali123 <94912062+muhammadsaifali123@users.noreply.github.com>
2025-04-13 20:50:24 +0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -43,7 +43,7 @@ cmd({
     const fileName = contentDisposition ? contentDisposition.match(/filename=(.*)/)[1] : `${repo}.zip`;
 
     // Notify user of the download
-    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *Powered by JawadTechX*`);
+    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *Pᴏᴡᴇʀᴇᴅ Bʏ ᴍᴏɪZ*`);
 
     // Send the zip file to the user with custom contextInfo
     await conn.sendMessage(from, {
@@ -56,7 +56,7 @@ cmd({
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363354023106228@newsletter',
-          newsletterName: 'JawadTechX',
+          newsletterName: 'MOIZ-MD',
           serverMessageId: 143
         }
       }
